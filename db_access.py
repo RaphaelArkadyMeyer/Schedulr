5177a9cd7a897322361da548d335fc458be98eb3
Raphael Arakdy Meyer <Meyer219@purdue.edu>
2016-11-27 17:48:27 -0500

    Added db_access.py
    Created cloudant client that uploads all of the json produced by
    get_courses.py stored in a local file and uploads it to 'testing_db' on
    Cloudant. We should probably rename testing_db to dev_db as a staged db
    service that we then replicate to the purdue_courses database.

---
+++ db_access.py
@@ -0,0 +1,80 @@
+from cloudant.client import Cloudant
+import json
+
+# HIDDEN FROM GITHUB. GET YOUR OWN KEY FROM BLUEMIX
+api_url  = 'https://user:pass@url-bluemix.cloudant.com'
+api_user = 'user'
+api_pass = 'pass'
+
+
+def queryFromDB(odata_id):
+    client = Cloudant(api_user, api_pass, url=api_url)
+    print("Quering all meeting times")
+    client.disconnect()
+
+    # Connect to the server
+    client.connect()
+
+    # Perform client tasks...
+    session = client.session()
+    print('Username: {0}'.format(session['userCtx']['name']))
+    print('Databases: {0}'.format(client.all_dbs()))
+
+    courses_db = client['testing_db']
+
+    if odata_id not in courses_db:
+        out = None
+    else:
+        out = courses_db[odata_id]
+
+    client.disconnect()
+    return out
+
+
+queryFromDB('8bee1343-3e01-42e0-bcbb-410a3c24bd77')
+
+
+def writeAllToDB():
+    client = Cloudant(api_user, api_pass, url=api_url)
+    # or using url
+    # client = Cloudant(USERNAME, PASSWORD, url='https://acct.cloudant.com')
+
+    # Connect to the server
+    client.connect()
+
+    # Perform client tasks...
+    session = client.session()
+    print('Username: {0}'.format(session['userCtx']['name']))
+    print('Databases: {0}'.format(client.all_dbs()))
+
+    courses_db = client['testing_db']
+
+    file_path = "CourseInfo.json"
+    f = open(file_path, 'r')
+    text = f.read()
+    f.close()
+    caches = json.loads(text)
+
+    messageSize = 100
+    for cache_type, cache in caches.items():
+        print("Uploading {}...".format(cache_type))
+        i = 1
+        size = len(cache.items())
+        load = list()
+        for item_id, item_json in cache.items():
+            if i % messageSize == 0:
+                print("{:.1f}%\t{}".format(100 * i / size, i))
+                rts = courses_db.bulk_docs(load)
+                for rt in rts:
+                    if 'error' in rt.keys():
+                        print('{} Failed with error: {}'.format(rt['id'], rt['error']))
+                load = list()
+            # print("id:{}\n\tjson:{}".format(item_id, item_json))
+            load.append(item_json)
+            # new_document = courses_db.create_document(item_json)
+            # if not new_document.exists():
+            #     print("{} failed to upload".format(item_id))
+            i = i + 1
+
+    # Disconnect from the server
+    client.disconnect()
